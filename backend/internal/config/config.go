5b7f9c28248ac9bb63bdf6f80c4ea22ab08402f2
divyanshu978 <divyanshuuttam978@gmail.com>
2025-10-01 22:48:33 +0530

    remove database credentials

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -28,9 +28,9 @@ func getEnv(key, fallback string) string {
 func Load() *AppConfig {
 	return &AppConfig{
 		Port:         getEnv("PORT", "8080"),
-		Neo4jURI:     getEnv("NEO4J_URI", "neo4j://127.0.0.1:7687"),
+		Neo4jURI:     getEnv("NEO4J_URI", "path"),
 		Neo4jUser:    getEnv("NEO4J_USER", "neo4j"),
-		Neo4jPass:    getEnv("NEO4J_PASS", "12345678"), // IMPORTANT: Change this or set env var
+		Neo4jPass:    getEnv("NEO4J_PASS", "password"), // IMPORTANT: Change this or set env var
 		ToolsPath:    getEnv("TOOLS_PATH", "../../../tools"), // Default for local dev
 		TempUploads:  getEnv("TEMP_UPLOADS", os.TempDir()), // Use system's temp dir
 	}
